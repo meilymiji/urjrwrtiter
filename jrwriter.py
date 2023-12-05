6f1139af594d1843989dd1babebce1e54bb07cfd
meilymiji <149761862+meilymiji@users.noreply.github.com>
2023-12-05 20:48:00 +0700

    Update jrwriter.py

--- jrwriter.py
+++ jrwriter.py
@@ -7,10 +7,10 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
 
-prompt = """Act as a high school student writing a paragraph in English. You will recieve a topic that contains the topic's objective and some background information. Write a paragraph that related to the topic. The paragraph should be at least 50 words. You can write more than one paragraph."""
+prompt = """Act as a high school student writing a paragraph in English. You will recieve a topic that contains the topic's objective and some background information. Write a paragraph that related to the topic then List the words that high school student should know in JSON array. Each word should have the part of speech of that word ,the difficulty of that word,  the meaning of that word and the example usage in other context. The paragraph should be at least 80 words. You can write more than one paragraph. The words should be at least 8 words."""
 
 st.title('Your Junior Writer')
-st.markdown('Input the topic. \n\ Do not forget to put an objective and some background information in the topic. :)')
+st.markdown('Input the topic. \n Do not forget to put an objective and some background information in the topic :)')
 
 user_input = st.text_area('Enter an objective and some background information')
 
